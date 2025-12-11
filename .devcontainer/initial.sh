9e7649f25ea72bead977570d9dc3a716bd1c96f8
Ryotaro Sakai <sakai@cloudbuilders.jp>
2025-12-11 02:21:50 +0000

    Update Kotlin LSP version in initial.sh to 261.13587.0

--- .devcontainer/initial.sh
+++ .devcontainer/initial.sh
@@ -3,4 +3,4 @@
 [ -f .extensions/kotlin.vsix ] && exit 0
 
 mkdir -p .extensions/
-curl -L -o .extensions/kotlin.vsix https://download-cdn.jetbrains.com/kotlin-lsp/0.253.10629/kotlin-0.253.10629.vsix
+curl -L -o .extensions/kotlin.vsix https://download-cdn.jetbrains.com/kotlin-lsp/261.13587.0/kotlin-lsp-261.13587.0-linux-aarch64.vsix
